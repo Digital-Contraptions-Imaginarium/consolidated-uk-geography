a76993ff4a48075ac36daec1690a0bc5367c42a5
Gianfranco Cecconi <giacecco@dico.im>
2015-07-02 19:41:46 +0100

    Adapted importing Scotland's population

--- process.sh
+++ process.sh
@@ -21,9 +21,14 @@ psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE EXTENSION IF NOT EXISTS po
 psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS esw_boundaries;"
 shp2pgsql -I -c -W "latin1" -s EPSG:27700 "source_data/great_britain/Local_authority_district_(GB)_2011_Boundaries_(Full_Extent)/LAD_DEC_2011_GB_BFE.shp" esw_boundaries | psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME
 
-# import population density data for England and Wales
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS ew_population;"
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE TABLE ew_population (lad11cd CHAR(9), all_usual_residents INTEGER, males INTEGER, females INTEGER, lives_in_an_household INTEGER, lives_in_a_communal_establishment INTEGER, schoolchild_or_fulltime_student_aged_4_and_blah_blah INTEGER, area REAL, density REAL);"
-csvfix exclude -f 1,2,4 "$(dir_resolve source_data/england_and_wales/ks101ew.csv)" | tail -n +2 | sed '$d' > .temp.csv
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"COPY ew_population (lad11cd, all_usual_residents, males, females, lives_in_an_household, lives_in_a_communal_establishment, schoolchild_or_fulltime_student_aged_4_and_blah_blah, area, density) FROM '$(dir_resolve .temp.csv)' WITH CSV;"
+# import population for England and Wales
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS ews_population;"
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE TABLE ews_population (lad11cd CHAR(9), all_usual_residents INTEGER, area REAL, density REAL);"
+csvfix exclude -f 1,2,4,5,6,7,8,9 "$(dir_resolve source_data/england_and_wales/ks101ew.csv)" | tail -n +2 | sed '$d' > .temp.csv
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"COPY ews_population (lad11cd, all_usual_residents, area, density) FROM '$(dir_resolve .temp.csv)' WITH CSV;"
+rm -rf .temp.csv
+
+# import population for Scotland
+csvfix edit -f 2,3 -e 's/,//g' source_data/scotland/Council\ Area\ blk/QS102SC.csv | tail -n +2 | sed '$d' > .temp.csv
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"COPY ews_population (lad11cd, all_usual_residents, area, density) FROM '$(dir_resolve .temp.csv)' WITH CSV;"
 rm -rf .temp.csv
