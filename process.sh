91e32e698257802a28499dbb1ea3f60e6513e709
Gianfranco Cecconi <giacecco@dico.im>
2015-07-02 22:32:03 +0100

    Better consistency of GB data, ongoing

--- process.sh
+++ process.sh
@@ -18,17 +18,21 @@ psql --set ON_ERROR_STOP=1 -dpostgres -c"CREATE DATABASE $DATABASE_NAME;"
 psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;"
 
 # import local authority boundaries for England, Scotland and Wales
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS esw_boundaries;"
-shp2pgsql -I -c -W "latin1" -s EPSG:27700 "source_data/great_britain/Local_authority_district_(GB)_2011_Boundaries_(Full_Extent)/LAD_DEC_2011_GB_BFE.shp" esw_boundaries | psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS gb_boundaries;"
+shp2pgsql -I -c -W "latin1" -s EPSG:27700 "source_data/great_britain/Local_authority_district_(GB)_2011_Boundaries_(Full_Extent)/LAD_DEC_2011_GB_BFE.shp" gb_boundaries | psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME
 
 # import population for England and Wales
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS ews_population;"
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE TABLE ews_population (lad11cd CHAR(9), all_usual_residents INTEGER, area REAL, density REAL);"
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"DROP TABLE IF EXISTS gb_population;"
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"CREATE TABLE gb_population (lad11cd CHAR(9), all_usual_residents INTEGER, area REAL, density REAL);"
 csvfix exclude -f 1,2,4,5,6,7,8,9 "$(dir_resolve source_data/england_and_wales/ks101ew.csv)" | tail -n +2 | sed '$d' > .temp.csv
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"COPY ews_population (lad11cd, all_usual_residents, area, density) FROM '$(dir_resolve .temp.csv)' WITH CSV;"
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"COPY gb_population (lad11cd, all_usual_residents, area, density) FROM '$(dir_resolve .temp.csv)' WITH CSV;"
 rm -rf .temp.csv
 
 # import population for Scotland
-csvfix edit -f 2,3 -e 's/,//g' source_data/scotland/Council\ Area\ blk/QS102SC.csv | tail -n +2 | sed '$d' > .temp.csv
-psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"COPY ews_population (lad11cd, all_usual_residents, area, density) FROM '$(dir_resolve .temp.csv)' WITH CSV;"
+# Note: the source data has a row for the Scotland total (Scotland's geography code is S92000003), so I need to drop
+#       that.
+# PROBLEM: the shapefile has a "Glasgow City" entry (S12000046), what is that?
+# PROBLEM: the Scottish population entries are 31, what is missing? it should be 32 https://en.wikipedia.org/wiki/Local_government_in_Scotland
+csvfix edit -f 2,3 -e 's/,//g' source_data/scotland/Council\ Area\ blk/QS102SC.csv | csvfix remove -f 1 -s 'S92000003' | tail -n +2 | sed '$d' > .temp.csv
+psql --set ON_ERROR_STOP=1 -d$DATABASE_NAME -c"COPY gb_population (lad11cd, all_usual_residents, area, density) FROM '$(dir_resolve .temp.csv)' WITH CSV;"
 rm -rf .temp.csv
